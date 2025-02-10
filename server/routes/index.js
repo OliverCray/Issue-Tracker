2de8d57bc816de67692409e6d339ffb72100c38f
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2025-02-10 15:09:19 +0000

    Added api prefix to auth and issues routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ import router from './routes/index.js'
 import authRoutes from './routes/authRoutes.js'
 import issueRoutes from './routes/issueRoutes.js'
 
-router.use('/auth', authRoutes)
-router.use('/issues', issueRoutes)
+router.use('/api/auth', authRoutes)
+router.use('/api/issues', issueRoutes)
 
 export default router
