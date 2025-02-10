658188d97336f14a2ea794f5c0a40fdb1da382f0
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2025-02-10 14:00:56 +0000

    Set up server connection

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import connection from './config/connection.js'
+import routes from './routes'
+
+dotenv.config()
+connection()
+
+const app = express()
+const server = createServer(app)
+export const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+
+io.on('connection', (socket) => {
+  console.log('Socket connected:', socket.id)
+})
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
